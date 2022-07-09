c917051134595528db5466bf7573bdc833358064
Juan Felipe Mesa Ocampo <jmesa@sistepass.com>
2022-07-09 15:56:43 -0500

    Se realizan ajustes para que el componente se pueda exportar y crear el archivo js minificado final.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component, Injector } from '@angular/core';
-import { createCustomElement } from '@angular/elements';
-import { SimuladorComponent } from './simulador/simulador.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-simulator',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(injector: Injector, public simulator: SimuladorComponent) {
-    // Convert `PopupComponent` to a custom element.
-    const appSimulator = createCustomElement(SimuladorComponent, {injector});
-    // Register the custom element with the browser.
-    customElements.define('app-simulator', appSimulator);
-  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
+import {Injector, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
 import { SimuladorComponent } from './simulador/simulador.component';
+import {createCustomElement} from "@angular/elements";
 
 @NgModule({
   declarations: [
@@ -15,10 +15,16 @@ import { SimuladorComponent } from './simulador/simulador.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(injector: Injector) {
+    // Convert `PopupComponent` to a custom element.
+    const appSimulator = createCustomElement(SimuladorComponent, {injector});
+    // Register the custom element with the browser.
+    customElements.define('app-simulator', appSimulator);
+  }
+}
